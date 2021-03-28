bf1e2fa2acfa2c753eba7852c8223a2651bfb89c
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-03-28 12:26:35 -0400

    updated style.css

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ const headings = document.getElementsByClassName("headings")[0]
 
 headings.innerText = "Welcome to my first Website"
 
+
+const getintouch = document.getElementsByid("getintouch")[0]
+
+getintouch.innerText = "Keep in touch";
+
+
+alert("Welcome to my website");
+
+
